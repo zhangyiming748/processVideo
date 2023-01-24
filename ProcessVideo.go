fa65285a155db54bd29712d4cedfe55425790785
zen <zhangyiming748@gmail.com>
2023-01-24 21:29:43 +0800

    手动输入线程 异步获取帧数

--- ProcessVideo.go
+++ ProcessVideo.go
@@ -2,12 +2,11 @@ package processVideo
 
 import (
 	"fmt"
+	"github.com/zhangyiming748/getInfo"
 	"github.com/zhangyiming748/log"
 	"github.com/zhangyiming748/processVideo/convert"
 	"github.com/zhangyiming748/processVideo/util"
 	"os"
-	"runtime"
-	"strconv"
 	"strings"
 	"time"
 )
@@ -22,17 +21,17 @@ const (
 	HUGE   = 500 * MB
 )
 
-func ProcessVideos(dir, pattern string) {
+func ProcessVideos(dir, pattern, threads string) {
 	m_start := time.Now()
 	start := time.Now().Format("整个任务开始时间 15:04:03")
 	log.Debug.Println(start)
 
-	thread := runtime.NumCPU() / 2
-	threads := strconv.Itoa(thread)
 	var files []util.File
 	files = util.GetFileInfo(util.GetMultiFiles(dir, pattern))
 	for _, file := range files {
-		frame := util.DetectFrame(file)
+		//frame := util.DetectFrame(file)
+		var frame int
+		go getInfo.GetVideoFrame(file.FullPath)
 		log.Debug.Printf("文件帧数约%d\n", frame)
 		if frame < 500 {
 			convert.Convert2AV1(file, threads)
@@ -60,13 +59,11 @@ func ProcessVideos(dir, pattern string) {
 	log.Debug.Printf("整个任务用时 %v 分\n", during)
 }
 
-func ProcessAllVideos(root, pattern string) {
+func ProcessAllVideos(root, pattern, threads string) {
 	m_start := time.Now()
 	start := time.Now().Format("整个任务开始时间 15:04:03")
 	log.Debug.Println(start)
-
-	thread := runtime.NumCPU() / 2
-	threads := strconv.Itoa(thread)
+	
 	var files []util.File
 	folders := listFolders(root)
 	for _, src := range folders {
