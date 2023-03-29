87275ef609563fff69b4fba5997453422ed03232
zen <zhangymb@adtec.com.cn>
2023-03-29 09:21:24 +0800

    fix

--- convert/h265.go
+++ convert/h265.go
@@ -23,31 +23,31 @@ func Convert2H265(in GetFileInfo.Info, threads string) {
 	out := strings.Join([]string{prefix, middle, in.FullName}, "")
 	defer func() {
 		if err := recover(); err != nil {
-			slog.Warn("出现错误", slog.Any("输入文件", in.FullPath), slog.Any("输出文件", out))
+			slog.Warn("出现错误", slog.String("输入文件", in.FullPath), slog.String("输出文件", out))
 		}
 	}()
 	mp4 := strings.Join([]string{strings.Trim(out, in.ExtName), "mp4"}, "")
-	slog.Debug("调试", slog.Any("输入文件", in.FullPath), slog.Any("输出文件", out))
+	slog.Debug("调试", slog.String("输入文件", in.FullPath), slog.String("输出文件", out))
 
 	cmd := exec.Command("ffmpeg", "-threads", threads, "-i", in.FullPath, "-c:v", "libx265", "-threads", threads, "-tag:v", "hvc1", mp4)
 	if runtime.GOOS == "darwin" {
-		slog.Debug("匹配到苹果设备,使用硬件加速", "https://developer.apple.com/documentation/videotoolbox")
+		slog.Debug("匹配到苹果设备,使用硬件加速", slog.String("文档", "https://developer.apple.com/documentation/videotoolbox"))
 		cmd = exec.Command("ffmpeg", "-threads", threads, "-hwaccel", "videotoolbox", "-i", in.FullPath, "-c:v", "libx265", "-threads", threads, "-tag:v", "hvc1", mp4)
 	}
 	// info := GetFileInfo.GetVideoFileInfo(in.FullPath)
 	if info.Width > 1920 && info.Height > 1920 {
-		slog.Debug("视频大于1080P需要使用其他程序先处理视频尺寸", in)
+		slog.Warn("视频大于1080P需要使用其他程序先处理视频尺寸", slog.Any("原视频", in))
 		return
 	}
-	slog.Debug("生成的命令", slog.Any("command", fmt.Sprint(cmd)))
+	slog.Debug("生成的命令", slog.String("command", fmt.Sprint(cmd)))
 	stdout, err := cmd.StdoutPipe()
 	cmd.Stderr = cmd.Stdout
 	if err != nil {
-		slog.Warn("cmd.StdoutPipe产生错误", err)
+		slog.Warn("cmd.StdoutPipe产生错误", slog.Any("产生错误", err))
 		return
 	}
 	if err = cmd.Start(); err != nil {
-		slog.Warn("cmd.Run产生的错误", err)
+		slog.Warn("cmd.Run产生的错误", slog.Any("产生错误", err))
 		return
 	}
 	for {
@@ -61,7 +61,7 @@ func Convert2H265(in GetFileInfo.Info, threads string) {
 		}
 	}
 	if err = cmd.Wait(); err != nil {
-		slog.Warn("命令执行中有错误产生", err)
+		slog.Warn("命令执行中有错误产生", slog.Any("产生错误", err))
 		return
 	}
 	//log.Debug.Printf("完成当前文件的处理:源文件是%s\t目标文件是%s\n", in, file)
@@ -73,7 +73,7 @@ func Convert2H265(in GetFileInfo.Info, threads string) {
 }
 func ConvertOne(src, dst, threads string) {
 	cmd := exec.Command("ffmpeg", "-threads", threads, "-i", src, "-c:v", "libx265", "-threads", threads, dst)
-	slog.Debug("生成的命令", slog.Any("command", fmt.Sprint(cmd)))
+	slog.Debug("生成的命令", slog.String("command", fmt.Sprint(cmd)))
 	stdout, err := cmd.StdoutPipe()
 	cmd.Stderr = cmd.Stdout
 	if err != nil {
@@ -95,12 +95,12 @@ func ConvertOne(src, dst, threads string) {
 		}
 	}
 	if err = cmd.Wait(); err != nil {
-		slog.Warn("命令执行出错", slog.Any("错误", err))
+		slog.Warn("命令执行出错", slog.Any("产生错误", err))
 		return
 	}
 	//log.Debug.Printf("完成当前文件的处理:源文件是%s\t目标文件是%s\n", in, file)
 	if err := os.RemoveAll(src); err != nil {
-		slog.Warn("删除失败", slog.Any("源文件", err))
+		slog.Warn("删除失败", slog.Any("源文件", src), slog.Any("产生错误", err))
 	} else {
 		slog.Info("删除成功", slog.Any("源文件", src))
 	}
--- go.mod
+++ go.mod
@@ -4,13 +4,11 @@ go 1.19
 
 require (
 	github.com/zhangyiming748/replace v0.0.7
-	github.com/zhangyiming748/voiceAlert v0.0.12
+	github.com/zhangyiming748/voiceAlert v0.0.13
 )
 
 require (
-	github.com/zhangyiming748/GetAllFolder v0.0.6
-	github.com/zhangyiming748/GetFileInfo v0.0.19
+	github.com/zhangyiming748/GetAllFolder v0.0.7
+	github.com/zhangyiming748/GetFileInfo v0.0.21
 	golang.org/x/exp v0.0.0-20230321023759-10a507213a29
 )
-
-require github.com/zhangyiming748/log v0.0.6 // indirect
